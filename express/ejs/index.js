f4480d276e9300c45e1de62961681005fdc84dfb
abhishek100sahu <abhishek97sahu@gmail.com>
2025-04-17 00:42:25 +0530

    ejs, img public, views/includes

--- express/ejs/index.js
+++ express/ejs/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -18,6 +20,18 @@ app.get("/ig/:username", (req, res) => {
   let { username } = req.params;
   let followers = ["abhishek", "nimesh", "abdur"];
   res.render("instagram", { username: username, followers: followers });
+});
+
+app.get("/instagram/:username", (req, res) => {
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+  console.log(data);
+  if (data) {
+    res.render("instagram.ejs", { data: data });
+  } else {
+    res.render("error.ejs");
+  }
 });
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
